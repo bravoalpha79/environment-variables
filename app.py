740d500c65ce489f5c65f240bb8573a78f6bf093
Igor Basuga <igor.basuga@gmail.com>
2020-06-10 17:39:26 +0000

    add pymongo functionality; add .gitignore

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask, render_template, request, flash
+from flask_pymongo import PyMongo
+
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -13,9 +25,10 @@ def home():
 @app.route("/send", methods=["GET", "POST"])
 def send():
     if request.method == "POST":
-        content = request.form.get("content")
-        flash(f"Thank you, your message '{content}' has been received!")
-    return render_template("home.html")
+        content = int(request.form.get("content"))
+        mongo.db.Numbers.insert_one({"number": content})
+        flash(f"Number '{content}' has been successfully inserted!")
+    return render_template("home.html", numbers=mongo.db.Numbers.find())
 
 
 if __name__ == "__main__":
